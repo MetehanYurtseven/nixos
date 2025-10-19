27d2352124c407908c33a7041799ce2478e127e9
Metehan Yurtseven <metehan.yurtseven@itsolutions-gg.de>
2025-10-20 00:20:29 +0200

    arch hyprland config migration finished

--- home.nix
+++ home.nix
@@ -110,21 +110,21 @@
       "$outlook" = "gtk-launch outlook"; # NOT INSTALLED
       "$ha" = "gtk-launch home-assistant"; # NOT INSTALLED 
 
-      # "$base" = "0xff191724";
-      # "$surface" = "0xff1f1d2e";
-      # "$overlay" = "0xff26233a";
-      # "$muted" = "0xff6e6a86";
-      # "$subtle" = "0xff908caa";
-      # "$text" = "0xffe0def4";
-      # "$love" = "0xffeb6f92";
-      # "$gold" = "0xfff6c177";
-      # "$rose" = "0xffebbcba";
-      # "$pine" = "0xff31748f";
-      # "$foam" = "0xff9ccfd8";
-      # "$iris" = "0xffc4a7e7";
-      # "$highlightLow" = "0xff21202e";
-      # "$highlightMed" = "0xff403d52";
-      # "$highlightHigh" = "0xff524f67";
+      "$base" = "0xff191724";
+      "$surface" = "0xff1f1d2e";
+      "$overlay" = "0xff26233a";
+      "$muted" = "0xff6e6a86";
+      "$subtle" = "0xff908caa";
+      "$text" = "0xffe0def4";
+      "$love" = "0xffeb6f92";
+      "$gold" = "0xfff6c177";
+      "$rose" = "0xffebbcba";
+      "$pine" = "0xff31748f";
+      "$foam" = "0xff9ccfd8";
+      "$iris" = "0xffc4a7e7";
+      "$highlightLow" = "0xff21202e";
+      "$highlightMed" = "0xff403d52";
+      "$highlightHigh" = "0xff524f67";
 
       monitorv2 = [
         {
@@ -166,6 +166,7 @@
       	"special:ha, on-created-empty:$ha"
       ];
 
+      # Autostart
       exec-once = [
       	"1password --silent &" # Password Manager
         # "waybar" # Menu Bar # TODO
@@ -175,21 +176,89 @@
       	# "thunar --daemon" # File Manager Daemon # TODO
       ];
 
+      # See https://wiki.hyprland.org/Configuring/Environment-variables/
       env = [
       	"XDG_CURRENT_DESKTOP,Hyprland"
       	"XCURSOR_SIZE,24"
       	"HYPRCURSOR_SIZE,24"
       ];
 
+      # https://wiki.hyprland.org/Configuring/Variables/#general
       general =  {
         gaps_in = 5;
         gaps_out = "10,20,20,20";
+
         border_size = 1;
-        # "col.active_border" = "$rose $pine $love $iris 90deg";
-        # "col.inactive_border" = "$muted";
-        resize_on_border = false;
-        allow_tearing = false;
+
+        "col.active_border" = "$rose $pine $love $iris 90deg";
+        "col.inactive_border" = "$muted";
+
+        resize_on_border = false; # Set to true enable resizing windows by clicking and dragging on borders and gaps
+        allow_tearing = false; # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
+
         layout = "dwindle";
+      };
+
+      # https://wiki.hyprland.org/Configuring/Variables/#decoration
+      decoration = {
+          rounding = 10;
+
+          # Change transparency of focused and unfocused windows
+          active_opacity = 1;
+          inactive_opacity = 0.8;
+
+          # https://wiki.hyprland.org/Configuring/Variables/#blur
+          blur = {
+              enabled = true;
+              size = 10;
+              passes = 3;
+              
+              vibrancy = 0.1696;
+          };
+      };
+
+      # Default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+      animations = {
+          enabled = true;
+          bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+          animation = [
+            "windows, 1, 5, myBezier"
+            "windowsOut, 1, 5, default, popin 80%"
+            "border, 1, 5, default"
+            "borderangle, 1, 5, default"
+            "fade, 1, 5, default"
+            "workspaces, 1, 5, default"
+          ];
+      };
+
+      # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
+      dwindle = {
+          pseudotile = true; # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+          preserve_split = true; # You probably want this
+      };
+
+      # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
+      master = {
+          new_status = "master";
+      };
+
+      # https://wiki.hyprland.org/Configuring/Variables/#misc
+      misc = { 
+          force_default_wallpaper = -1; # Set to 0 or 1 to disable the anime mascot wallpapers
+          disable_hyprland_logo = false; # If true disables the random hyprland logo / anime girl background. :(
+      };
+
+      input = {
+        kb_layout = "de";
+        kb_options = "ctrl:nocaps";
+        follow_mouse = 1;
+        sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
+        touchpad.natural_scroll = false;
+      };
+
+      device = {
+        name = "logitech-mx-master-3s";
+        sensitivity = -1;
       };
 
       bind = [
@@ -266,16 +335,80 @@
         # Scroll through existing workspaces with mainMod + scroll
         "$mod, mouse_up, workspace, e+1"
         "$mod, mouse_down, workspace, e-1"
+
+
       ];
 
+      # Move/resize windows with mainMod + LMB/RMB and dragging
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
 
-      input = {
-        kb_layout = "de";
-        kb_options = "ctrl:nocaps";
-        follow_mouse = 1;
-        sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
-        touchpad.natural_scroll = false;
-      };
+      # Laptop multimedia keys for volume and LCD brightness
+      bindel = [
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
+        ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
+      ];
+
+      # Windows Rules
+      windowrulev2 = [
+        "suppressevent maximize, class:.* # You'll probably like this."
+
+        # 1password
+        "float, title:(1Password)"
+        "size 70% 70%, title:(1Password)"
+        "center, title:(1Password)"
+
+        # pavucontrol
+        "float, title:(Volume Control)"
+        "size 50% 50%, title:(Volume Control)"
+        "center, title:(Volume Control)"
+
+        # Youtube Music
+        "float, title:(YouTube Music)"
+        "size 70% 70%, title:(YouTube Music)"
+        "center, title:(YouTube Music)"
+
+        # Microsoft Teams
+        "float, class:(chrome-teams.microsoft.com__-Default)"
+        "size 70% 70%, class:(chrome-teams.microsoft.com__-Default)"
+        "center, class:(chrome-teams.microsoft.com__-Default"
+
+        # Microsoft Outlook
+        "float, class:(chrome-outlook.office.com__-Default)"
+        "size 80% 80%, class:(chrome-outlook.office.com__-Default)"
+        "center, class:(chrome-outlook.office.com__-Default)"
+
+        # Home Assistant
+        "float, class:(chrome-home.melthrox.de__-Default)"
+        "size 63% 70%, class:(chrome-home.melthrox.de__-Default)"
+        "center, class:(chrome-home.melthrox.de__-Default)"
+
+        # WhatsApp
+        "float, class:(chrome-web.whatsapp.com__-Default)"
+        "size 70% 70%, class:(chrome-web.whatsapp.com__-Default)"
+        "center, class:(chrome-web.whatsapp.com__-Default)"
+
+        # Qalculate
+        "float, title:(Qalculate!)"
+        "size 40% 50%, title:(Qalculate!)"
+        "center, title:(Qalculate!)"
+
+        # Slite
+        "float, class:(chrome-itsolutions-gg.slite.com__app-Default)"
+        "size 80% 80%, class:(chrome-itsolutions-gg.slite.com__app-Default)"
+        "center, class:(chrome-itsolutions-gg.slite.com__app-Default)"
+
+        # Waypaper
+        "float, title:(Waypaper)"
+        "size 30% 50%, title:(Waypaper)"
+        "center, title:(Waypaper)"
+      ];
     };
   };
   home.sessionVariables.NIXOS_OZONE_WL = "1";
