e76ee9ca5d2c17e2629434493ad60e12231f158c
Metehan Yurtseven <metehan.yurtseven@itsolutions-gg.de>
2025-10-27 21:46:45 +0100

    packages updated

--- hosts/desktop/networking.nix
+++ hosts/desktop/networking.nix
@@ -8,5 +8,26 @@ in
   networking.firewall.enable = false;
   
   services.openssh.enable = true;
+
+  networking.wireguard = {
+    enable = true;
+    interfaces = {
+      wg0 = {
+        ips = [ "10.0.0.9" ];
+        # Nutze verschlüsselte Secrets
+        privateKeyFile = config.sops.secrets."wireguard/privateKey".path;
+        peers = [
+          {
+            publicKey = "m54PyD6ptUetZy17pExI6KnCvLumZEboXfd7YuSDVgA=";
+            presharedKeyFile = config.sops.secrets."wireguard/presharedKey".path;
+            allowedIPs = [
+              "10.0.0.0/16"
+            ];
+            endpoint = "vpn.melthrox.de:51821";
+          }
+        ];
+      };
+    };
+  };
 }
 
