cb8ba53e7425762c517123be1fa4edcdf2708c78
Metehan Yurtseven <metehan.yurtseven@itsolutions-gg.de>
2025-11-02 22:07:21 +0100

    solves GG-71: xonsh aliases

--- system/xonsh.nix
+++ system/xonsh.nix
@@ -7,11 +7,35 @@
       if 'SSH_TTY' not in os.environ:
           $SSH_AUTH_SOCK = $HOME + "/.1password/agent.sock"
       
-      execx($(zoxide init xonsh))
+      # Aliase aus zsh-Config
+      aliases.update({
+          # Navigation
+          'cd': ['z'],
+          
+          # ls-basierte Aliase
+          'ls': ['lsd'],
+          'sl': ['lsd'],
+          'l': ['lsd'],
+          'll': ['lsd', '-l'],
+          'la': ['lsd', '-lA'],
+          'lt': ['lsd', '-l', '--tree', '--depth', '3'],
+          'tree': ['lsd', '--tree'],
+          
+          # NixOS System-Aliase
+          'update': ['sudo', 'nixos-rebuild', 'switch', '--recreate-lock-file', '--flake', '/etc/nixos'],
+          'clean': ['bash', '-c', 'sudo nix profile wipe-history --profile /nix/var/nix/profiles/system --older-than 7d && sudo nix-collect-garbage --delete-old'],
+          'switch': ['sudo', 'nixos-rebuild', 'switch'],
+          'test': ['sudo', 'nixos-rebuild', 'test']
+      })
     '';
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableXonshIntegration = true;
   };
   
   environment.systemPackages = with pkgs; [
-    zoxide
+    lsd
   ];
 }
