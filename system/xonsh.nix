51d9a7a12918134dd35df4db62b1b61b6ccd1ec9
Metehan Yurtseven <metehan.yurtseven@itsolutions-gg.de>
2025-11-02 21:47:13 +0100

    GG-69: z installed

--- system/xonsh.nix
+++ system/xonsh.nix
@@ -6,6 +6,12 @@
       import os
       if 'SSH_TTY' not in os.environ:
           $SSH_AUTH_SOCK = $HOME + "/.1password/agent.sock"
+      
+      execx($(zoxide init xonsh))
     '';
   };
+  
+  environment.systemPackages = with pkgs; [
+    zoxide
+  ];
 }
