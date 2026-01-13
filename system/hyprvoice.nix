eef67607144d1ea21d1b651a5f10d8adbe131864
Metehan Yurtseven <metehan.yurtseven@itsolutions-gg.de>
2026-01-13 22:27:04 +0100

    solves GG-185: hyprvoice bug

--- system/hyprvoice.nix
+++ system/hyprvoice.nix
@@ -9,16 +9,23 @@
   systemd.user.services.hyprvoice = {
     description = "Hyprvoice voice-to-text daemon";
     documentation = [ "https://github.com/leonardotrapani/hyprvoice" ];
-    after = [ "pipewire.service" ];
-    wants = [ "pipewire.service" ];
+    after = [
+      "pipewire.service"
+      "wayland-session-waitenv.service"
+    ];
+    requires = [ "wayland-session-waitenv.service" ];
     wantedBy = [ "graphical-session.target" ];
     partOf = [ "graphical-session.target" ];
 
     serviceConfig = {
       Type = "simple";
-      ExecStart = "${pkgs.hyprvoice}/bin/hyprvoice serve";
+      ExecStart = "${pkgs.uwsm}/bin/uwsm app -- ${pkgs.hyprvoice}/bin/hyprvoice serve";
       Restart = "on-failure";
       RestartSec = 5;
+      PassEnvironment = [
+        "WAYLAND_DISPLAY"
+        "XDG_CURRENT_DESKTOP"
+      ];
     };
   };
 }
