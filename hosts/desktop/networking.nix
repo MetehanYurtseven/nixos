0400d8a8456ab47d2387f5c5ba34ce8d1a827e9c
Metehan Yurtseven <metehan.yurtseven@itsolutions-gg.de>
2026-01-13 13:54:46 +0100

    some network improvements

--- hosts/desktop/networking.nix
+++ hosts/desktop/networking.nix
@@ -5,49 +5,56 @@
   ...
 }:
 {
-  networking.hostName = settings.system.hostname;
-  networking.firewall.enable = false;
+  networking = {
+    hostName = settings.system.hostname;
+    firewall.enable = false;
 
-  networking.wireless.enable = false;
-  networking.enableIPv6 = false;
+    wireless.enable = false;
+    enableIPv6 = false;
 
-  networking.defaultGateway = "192.168.0.1";
-  networking.nameservers = [
-    "192.168.0.1"
-    "1.1.1.1"
-    "8.8.8.8"
-  ];
-
-  networking.interfaces.enp5s0 = {
-    useDHCP = false;
-    ipv4.addresses = [
-      {
-        address = "192.168.0.100";
-        prefixLength = 24;
-      }
+    defaultGateway = "192.168.0.1";
+    nameservers = [
+      "192.168.0.1"
+      "1.1.1.1"
+      "8.8.8.8"
     ];
-    wakeOnLan.enable = true;
-  };
 
-  networking.wireguard = {
-    enable = true;
-    interfaces = {
-      wg0 = {
-        ips = [ "10.0.0.9" ];
-        privateKeyFile = config.sops.secrets."wireguard/private_key".path;
-        dynamicEndpointRefreshSeconds = 25;
-        mtu = 1280;
-        peers = [
-          {
-            publicKey = "m54PyD6ptUetZy17pExI6KnCvLumZEboXfd7YuSDVgA=";
-            presharedKeyFile = config.sops.secrets."wireguard/preshared_key".path;
-            allowedIPs = [
-              "10.0.0.0/16"
-            ];
-            endpoint = "vpn.melthrox.de:51821";
-            persistentKeepalive = 25;
-          }
-        ];
+    interfaces."enp5s0" = {
+      useDHCP = false;
+      ipv4.addresses = [
+        {
+          address = "192.168.0.100";
+          prefixLength = 24;
+        }
+      ];
+      wakeOnLan.enable = true;
+    };
+
+    resolvconf.extraOptions = [
+      "single-request"
+      "no-aaaa"
+    ];
+
+    wireguard = {
+      enable = true;
+      interfaces = {
+        wg0 = {
+          ips = [ "10.0.0.9" ];
+          privateKeyFile = config.sops.secrets."wireguard/private_key".path;
+          dynamicEndpointRefreshSeconds = 25;
+          mtu = 1280;
+          peers = [
+            {
+              publicKey = "m54PyD6ptUetZy17pExI6KnCvLumZEboXfd7YuSDVgA=";
+              presharedKeyFile = config.sops.secrets."wireguard/preshared_key".path;
+              allowedIPs = [
+                "10.0.0.0/16"
+              ];
+              endpoint = "vpn.melthrox.de:51821";
+              persistentKeepalive = 25;
+            }
+          ];
+        };
       };
     };
   };
